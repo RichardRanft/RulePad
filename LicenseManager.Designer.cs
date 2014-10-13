0af30432bf31dadd44633e90a361649d9cff6487
RichardRanft <vegassparkyrich@gmail.com>
2014-10-13 12:02:26 -0700

    Adding license management tool.

---
+++ LicenseManager.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace RulePad
+{
+    partial class LicenseManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tscbLicenseList = new System.Windows.Forms.ToolStripComboBox();
+            this.licensesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToScriptsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.doneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtbLicenseEditor = new System.Windows.Forms.RichTextBox();
+            this.ofdOpenLicenseFile = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSaveLicenseFile = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.licensesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(747, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tscbLicenseList});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(747, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tscbLicenseList
+            // 
+            this.tscbLicenseList.DropDownWidth = 200;
+            this.tscbLicenseList.Name = "tscbLicenseList";
+            this.tscbLicenseList.Size = new System.Drawing.Size(121, 25);
+            // 
+            // licensesToolStripMenuItem
+            // 
+            this.licensesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.addToScriptsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.doneToolStripMenuItem});
+            this.licensesToolStripMenuItem.Name = "licensesToolStripMenuItem";
+            this.licensesToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.licensesToolStripMenuItem.Text = "Licenses";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // addToScriptsToolStripMenuItem
+            // 
+            this.addToScriptsToolStripMenuItem.Name = "addToScriptsToolStripMenuItem";
+            this.addToScriptsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addToScriptsToolStripMenuItem.Text = "Add To Scripts";
+            this.addToScriptsToolStripMenuItem.Click += new System.EventHandler(this.addToScriptsToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // doneToolStripMenuItem
+            // 
+            this.doneToolStripMenuItem.Name = "doneToolStripMenuItem";
+            this.doneToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.doneToolStripMenuItem.Text = "Done";
+            // 
+            // rtbLicenseEditor
+            // 
+            this.rtbLicenseEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbLicenseEditor.Location = new System.Drawing.Point(0, 49);
+            this.rtbLicenseEditor.Name = "rtbLicenseEditor";
+            this.rtbLicenseEditor.Size = new System.Drawing.Size(747, 372);
+            this.rtbLicenseEditor.TabIndex = 2;
+            this.rtbLicenseEditor.Text = "";
+            // 
+            // ofdOpenLicenseFile
+            // 
+            this.ofdOpenLicenseFile.FileName = "*.txt";
+            // 
+            // LicenseManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 421);
+            this.Controls.Add(this.rtbLicenseEditor);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "LicenseManager";
+            this.Text = "LicenseManager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem licensesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToScriptsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem doneToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox tscbLicenseList;
+        private System.Windows.Forms.RichTextBox rtbLicenseEditor;
+        private System.Windows.Forms.OpenFileDialog ofdOpenLicenseFile;
+        private System.Windows.Forms.SaveFileDialog sfdSaveLicenseFile;
+    }
+}
---
+++ LicenseManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RulePad
+{
+    public partial class LicenseManager : Form
+    {
+        public LicenseManager()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(ofdOpenLicenseFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // load license text for review/edit
+                rtbLicenseEditor.LoadFile(ofdOpenLicenseFile.FileName);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sfdSaveLicenseFile.ShowDialog();
+        }
+
+        private void addToScriptsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // add license to list of licenses to prefix to output scripts
+        }
+    }
+}
