df73cd506f9597b4041c7bf96f2cc2b02d32130a
RichardRanft <vegassparkyrich@gmail.com>
2014-10-13 14:16:34 -0700

    WIP license system

--- LicenseInfo.cs
+++ LicenseInfo.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace RulePad
 {
     class LicenseInfo
     {
         private String m_fileName;
+        private String m_LicenseName;
         private List<String> m_licenseText;
         private bool m_plainText;
 
@@ -55,6 +58,71 @@ namespace RulePad
         public void Add(String textLine)
         {
             m_licenseText.Add(textLine);
+        }
+
+        public void Load(String fileName)
+        {
+            m_fileName = fileName;
+            m_LicenseName = StripFileName(fileName);
+            if (fileName.Contains(".txt"))
+            {
+                m_plainText = true;
+                LoadText();
+            }
+            else
+            {
+                m_plainText = false;
+            }
+        }
+
+        private void LoadRTF()
+        {
+            // not implemented yet
+            MessageBox.Show("Direct loading RTF files not yet implemented.", "Not Implemented");
+        }
+
+        private void LoadText()
+        {
+            Clear();
+            StreamReader license = null;
+            try
+            {
+                license = new StreamReader(@m_fileName);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "File Error");
+                return;
+            }
+            try
+            {
+                String line;
+                while((line = license.ReadLine()) != null)
+                {
+                    Add(line);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "File Read Error");
+            }
+        }
+
+        private String StripFileName(String name)
+        {
+            int slashIndex = 0;
+            int dotIndex = 0;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i].ToString() == "\\")
+                    slashIndex = i + 1;
+            }
+            for (int j = 0; j < name.Length; j++)
+            {
+                if (name[j].ToString() == ".")
+                    dotIndex = j;
+            }
+            return name.Substring(slashIndex, dotIndex - slashIndex);
         }
     }
 }
